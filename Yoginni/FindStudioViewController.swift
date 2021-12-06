3c199ed2011aa4a0ff8bd4b0d0d6172e0fd733bc
SROO0524 <vmflstm847@naver.com>
2021-12-07 00:59:52 +0900

    1. test

--- Yoginni/FindStudioViewController.swift
+++ Yoginni/FindStudioViewController.swift
@@ -12,6 +12,6 @@ class FindStudioViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .white
+        view.backgroundColor = .systemOrange
     }
 }
